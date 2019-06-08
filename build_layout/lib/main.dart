4f3cc18628a48758e8f422f4dedb73017c418a72
fanqi_company <fanqi_0817@live.com>
2019-06-08 11:11:03 +0800

    添加手势

--- build_layout/lib/main.dart
+++ build_layout/lib/main.dart
@@ -102,11 +102,12 @@ class TitleContainer extends StatelessWidget {
                 ],
               ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text("41"),
+          // Icon(
+          //   Icons.star,
+          //   color: Colors.red[500],
+          // ),
+          // Text("41"),
+          FavoriteWidget(isFavorite: true, count: 20,),
         ],
       ),
     );
@@ -136,7 +137,7 @@ class OperationContainer extends StatelessWidget {
 
   @pragma("Private")
   Widget buildMyButton(BuildContext context, IconData icon, String title) {
-    final color = Theme.of(context).primaryColor;
+    final Color color = Theme.of(context).primaryColor;
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
       // decoration: BoxDecoration(
@@ -159,7 +160,7 @@ class OperationContainer extends StatelessWidget {
 }
 
 class TextContainer extends StatelessWidget {
-  final text = "Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps."
+  final String text = "Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps."
   " Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes."
   " A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest,"
   " leads you to the lake, which warms to 20 degrees Celsius in the summer."
@@ -175,6 +176,57 @@ class TextContainer extends StatelessWidget {
       //   )
       // ),
       child: Text(text, softWrap: true),
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  FavoriteWidget({Key key, this.count, this.isFavorite}) : super(key: key);
+  
+  int count;
+  bool isFavorite;
+
+  @override
+  State<StatefulWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  int _favoriteCount;
+
+  void _toggleFavorite(TapUpDetails tapDetail) {
+    setState(() {
+      widget.isFavorite = !widget.isFavorite;
+      if (widget.isFavorite) {
+        _favoriteCount++;
+      } else {
+        _favoriteCount--;
+      }
+      widget.count = _favoriteCount;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _favoriteCount = widget.count;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapUp: _toggleFavorite,
+      child: Row(
+        children: <Widget>[
+          Icon(
+            widget.isFavorite ? Icons.star : Icons.star_border,
+            color: Colors.red[500],
+          ),
+          SizedBox(
+            width: 20.0,
+            child: Text("$_favoriteCount"),
+          ),
+        ],
+      ),
     );
   }
 }
